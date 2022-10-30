40dde536485df9e7737f545c51cb860fcc41a0e5
ganoodles <swajabbadaja@gmail.com>
2022-10-30 16:41:05 -0700

    Clean up cogs origins

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,3 @@
-from operator import truediv
-from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -15,24 +13,15 @@ app = FastAPI()
 with open("config.toml", "rb") as f:
     config = tomllib.load(f)
 
-origins = [
-    "http://localhost:" + str(config["ports"]["web"]),
-    "https://localhost" + str(config["ports"]["web"]),
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
 thumbnailFolder = os.path.dirname(config["path"]) + "/thumbnails"
-
 # start thumbnail watchdog
-# DirUtils.syncDirs(thumbnailFolder, config["path"], "thumbnails", "files", 25)
 p = Process(target=WatchDir, args=(thumbnailFolder, config["path"], 25))
 p.daemon = True
 p.start()
